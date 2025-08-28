ef0c3817014301b5d1368e3f1c0a1a1482877c77
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-28 21:29:39 +0600

    fixed: pnpm run build

--- src/main/private-chat/private-chat.module.ts
+++ src/main/private-chat/private-chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrivateChatService } from './private-chat.service';
 import { PrivateChatController } from './private-chat.controller';
 import { PrivateChatGateway } from './privateChatGateway/privateChatGateway';
+import { FileService } from 'src/lib/utils/file.service';
 
 @Module({
   controllers: [PrivateChatController],
-  providers: [PrivateChatService, PrivateChatGateway],
+  providers: [PrivateChatService, PrivateChatGateway, FileService],
 })
 export class PrivateChatModule {}
