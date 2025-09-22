909710bbca5d540ee0dd7321621cd64ee24cda78
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-09-22 21:41:58 +0600

    Added: service created

--- src/main/auth/auth.controller.ts
+++ src/main/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
+
 import {
   Body,
   Controller,
@@ -58,7 +58,6 @@ export class AuthController {
       uploadedUrl?.url || null,
     );
   }
-  
 
   // User login
   @Post('login')
