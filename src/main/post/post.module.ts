116ef3c3c665977da5af5c3558a373b63d8bdb60
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-04 09:56:23 +0600

    add: include CloudinaryService in PostModule providers

--- src/main/post/post.module.ts
+++ src/main/post/post.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { CreatePostService } from './services/post-create.service';
+import { CloudinaryService } from 'src/lib/cloudinary/cloudinary.service';
 
 @Module({
   controllers: [PostController],
-  providers: [CreatePostService],
+  providers: [CreatePostService, CloudinaryService],
 })
 export class PostModule {}
