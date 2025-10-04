e972e8a08533252d9b703c5eb9f02ad73e91fcc6
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-04 09:59:28 +0600

    feat: add PostCategory module with controller, service, and DTOs

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrivateChatModule } from './private-chat/private-chat.module';
 import { NotificationModule } from './notification/notification.module';
 import { PostModule } from './post/post.module';
+import { PostCategoryModule } from './admin/post-category/post-category.module';
 
 @Module({
-  imports: [AuthModule, PrivateChatModule, NotificationModule, PostModule],
+  imports: [AuthModule, PrivateChatModule, NotificationModule, PostModule, PostCategoryModule],
   controllers: [],
   providers: [],
 })
